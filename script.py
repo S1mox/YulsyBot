25c806de481bcbddd831d1d7cd9b8e94473d9853
S1mox <smoxgg@gmail.com>
2020-06-08 18:52:11 +0300

    logging_added

--- script.py
+++ script.py
@@ -1,1 +1,15 @@
-print('Hello, world!')
+from vk_api.longpoll import VkLongPoll,VkEventType
+from datetime import datetime as time
+
+import vk_api
+import config
+
+vk_session = vk_api.VkApi(token=config.VK_TOKEN)
+longpoll = VkLongPoll(vk_session)
+
+print("Module was started")
+while True:
+  for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+      print(f'TEXT: {event.text}')
+      print(f'TIME: {str(time.strftime(time.now(), "%H:%M:%S"))}')
