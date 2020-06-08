1de85e303bd59fc18c08f7f110d7ebb21a1173c5
S1mox <smoxgg@gmail.com>
2020-06-09 00:19:02 +0300

    v.0.0.3

--- handlers.py
+++ handlers.py
@@ -1,6 +1,5 @@
 from vk_api.longpoll import VkLongPoll,VkEventType
 
-from datetime import datetime as time
 import vk_api
 
 class MessageHandler:
@@ -8,5 +7,6 @@ class MessageHandler:
     self.vk_session = vk_session
     self.session_api = session_api
   
-  def send_message(self, user_id, message = ' ', random_id = '0', keyboard = None):
-    self.vk_session.method('messages.send', values= {'user_id':f'{user_id}', 'message': f"{message}", 'random_id':{random_id}, 'keyboard':f"{keyboard}"})
+  def send_message(self, message_type, id, message = ' ', random_id = '0', keyboard = None):
+      self.vk_session.method('messages.send', values= {f"{message_type}":f'{id}', 'message': f"{message}", 'random_id':{random_id}, 'keyboard':f"{keyboard}"})
+
--- script.py
+++ script.py
@@ -1,12 +1,15 @@
 from vk_api.longpoll import VkLongPoll,VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+
 from datetime import datetime as time
 
+import logging
 import vk_api
 import handlers
 import config
 
 contributors = ['Seamo', 'Nyalen']    # Developers
+logging.basicConfig(filename="Yul.log", filemode='w',level=logging.INFO)
 
 def create_keyboard():
   """ Создание клавиатуры в диалоге с пользователем """
@@ -24,32 +27,38 @@ vk_session = vk_api.VkApi(token=config.VK_TOKEN)
 session_api = vk_session.get_api()
 longpoll = VkLongPoll(vk_session)
 
-print(f"{'-' * 20}\n{'Session was started': ^20}")
+logging.info(f"{'-' * 20}\n{'Session was started': ^20}")
 while True:                       # бесконечное прослушивание ответов с серверов VK
   for event in longpoll.listen():     # собирает события с прослушки
+    logging.info(f"{event.type}; {event.from_chat}")
+
     if event.type == VkEventType.MESSAGE_NEW:      # если это новое сообщение, то =>
       handler = handlers.MessageHandler(vk_session, session_api)  # помощник для работы с сообщениями
 
-      if event.from_user and not event.from_me:
+      message = event.text.lower() # сохраняем сообщение пользователя
+
+      if event.from_user and not event.from_me:         # для диалога с пользователями
         keyboard = create_keyboard() # клавиатура для пользователя
-        message = event.text.lower() # сохраняем сообщение пользователя
 
         if message == 'Начать':                        
-          handler.send_message(event.user_id, "Давай начнем!", 0, keyboard) # при начале работы с ботессой
+          handler.send_message('user_id',event.user_id, "Давай начнем!", 0, keyboard) # при начале работы с ботессой
         elif message == 'кто твой создатель?': 
-          handler.send_message(event.user_id, f"У меня {len(contributors)} создателя. Yul powered by {' and '.join(contributors)}", 0, keyboard) # пасхалОчка
+          handler.send_message('user_id',event.user_id, f"У меня {len(contributors)} создателя. Yul powered by {' and '.join(contributors)}", 0, keyboard) # пасхалОчка
         elif message == 'какой сегодня день?':
-          handler.send_message(event.user_id, f'Сегодня: {str(time.date)}', random_id=0, keyboard = keyboard)
+          handler.send_message('user_id',event.user_id, f'Сегодня: {time.now().strftime("%d.%m.%Y")}', 0, keyboard)
         elif message == 'как я?':
-          handler.send_message(event.user_id, f'Я в норме!', random_id=0, keyboard = keyboard)
+          handler.send_message('user_id',event.user_id, f'Я в норме!', 0, keyboard)
         elif message == 'помощь':
-          handler.send_message(event.user_id, f'Пока я только узнаю, чем я могу помочь, простии 👉🏻👈🏻', random_id=0, keyboard = keyboard)
+          handler.send_message('peer_id',event.user_id, f'Пока я только узнаю, чем я могу помочь, простиии 👉🏻👈🏻', 0, keyboard)
         else:
           # отправим эхо, если не распознали запроса пользователя
-          handler.send_message(event.user_id, f"{event.text}", 0, keyboard)
-        
+          handler.send_message('user_id', event.user_id, f"{event.text}", 0, keyboard)       
+      elif event.from_chat and not event.from_me:                             # для чатов 
+        handler.send_message('chat_id', event.chat_id, f"{event.text}")
+      
+      if not event.from_me:
         # сохранение сообщения пользователя в логи
-        print(f'TEXT: {event.text}')
-        print(f'TIME: {str(time.strftime(time.now(), "%H:%M:%S"))}')
+        logging.info(f'TEXT: {message.encode("utf-8")}; TIME  : {str(time.strftime(time.now(), "%H:%M:%S"))}')
+
 else:
-  print(f"\n{'-' * 20}\n{'Session was started': ^20}")
+  logging.info(f"\n{'-' * 20}\n{'Session was started': ^20}")
